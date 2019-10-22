8ea0c061bce463df78f9a1b23ceddb4b1b68bdf3
Billy Koech <bkkoech@college.harvard.edu>
2019-10-22 12:28:02 -0400

    published app

--- Test2/app.R
+++ Test2/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("test Geyser Data"),
+    titlePanel("Chris be like"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
